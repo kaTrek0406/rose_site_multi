3cdd04f2bbbe795a26487b1855f2e07af65dd800
vladslav <maksimkala83@gmail.com>
2026-02-12 13:24:05 +0200

    Fix: Add basename to BrowserRouter for GH Pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ initializeTracking()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
